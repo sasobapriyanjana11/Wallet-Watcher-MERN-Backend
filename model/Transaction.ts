e5717d7500f766859d3baab06003aa438291539b
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-02-17 15:18:28 +0530

    change Transaction model

--- model/Transaction.ts
+++ model/Transaction.ts
@@ -1,10 +1,10 @@
-export default class Transaction {
+export interface Transaction {
     id?: number;  // Auto-incremented primary key in MySQL
-    userId!: number;  // Foreign key reference to User
-    categoryId!: number;  // Foreign key reference to Category
-    type!: "income" | "expense";  // Enum type
-    amount!: number;  // Decimal value
-    date!: Date;  // Default to current date
+    userId: number;  // Foreign key reference to User
+    categoryId: number;  // Foreign key reference to Category
+    type: "income" | "expense";  // Enum type
+    amount: number;  // Decimal value
+    date: Date;  // Default to current date
     description?: string;  // Optional field
     createdAt?: Date;  // Automatically handled by Prisma
     updatedAt?: Date;  // Automatically updated
